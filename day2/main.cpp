dcf81a6c37610dfae79ea6f96274872740cf112e
Kellan Clark <kellanclark22@gmail.com>
2021-12-02 06:19:03 -0500

    Compact day 2

--- day2/main.cpp
+++ day2/main.cpp
@@ -24,12 +24,14 @@ int main() {
 		input.push_back({readString, readNum});
 	}
 
-	// Part 1
+	// Part 1/2
 	int horizontal = 0;
 	int depth = 0;
+	int depth2 = 0;
 	for (int i = 0; i < input.size(); i++) {
 		if (input[i].command.compare("forward") == 0) {
 			horizontal += input[i].num;
+			depth2 += depth * input[i].num;
 		} else if (input[i].command.compare("down") == 0) {
 			depth += input[i].num;
 		} else if (input[i].command.compare("up") == 0) {
@@ -37,22 +39,7 @@ int main() {
 		}
 	}
 	printf("Part 1: %d\n", horizontal * depth);
-
-	// Part 2
-	horizontal = 0;
-	depth = 0;
-	int aim = 0;
-	for (int i = 0; i < input.size(); i++) {
-		if (input[i].command.compare("forward") == 0) {
-			horizontal += input[i].num;
-			depth += aim * input[i].num;
-		} else if (input[i].command.compare("down") == 0) {
-			aim += input[i].num;
-		} else if (input[i].command.compare("up") == 0) {
-			aim -= input[i].num;
-		}
-	}
-	printf("Part 2: %d\n", horizontal * depth);
+	printf("Part 2: %d\n", horizontal * depth2);
 
 	return 0;
 }
